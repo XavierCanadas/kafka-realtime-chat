cb861a5871b6e387354edd45a04bb1b45597af54
XavierCanadas <xavier.canadas01@estudiant.upf.edu>
2025-04-27 23:41:40 +0200

    Minor changes in the channel manager. Now it doesn't use the jwt token

--- fastapi_kafka/channel_manager/app/main.py
+++ fastapi_kafka/channel_manager/app/main.py
@@ -16,7 +16,6 @@ from fastapi.responses import HTMLResponse
 from contextlib import asynccontextmanager
 from sqlmodel import inspect, select
 
-from .jwt_auth import oauth2_scheme, get_username_from_token
 from .database import SessionDep, engine, get_channels_from_user, get_channels_by_name, create_channel, join_channel
 from .models import Channel, UserChannels
 
@@ -42,22 +41,12 @@ def check_db():
 
 # given a user, return all the channels
 @app.get("/channels/me")
-def get_user_channels(token: Annotated[str, Depends(oauth2_scheme)], session: SessionDep):
+def get_user_channels(username: str, session: SessionDep):
     """
     This endpoint returns all user's channels. When a user enters the app, calls this endpoint.
     params:
-        token Annotated[str, Depends(oauth2_scheme)]: the jwt token given by the login server
+        username: The username of the user
     """
-
-    # get username and check token
-    try:
-        username = get_username_from_token(token)
-    except Exception as error:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
 
     try:
         channels = get_channels_from_user(username, session)
@@ -69,7 +58,7 @@ def get_user_channels(token: Annotated[str, Depends(oauth2_scheme)], session: Se
 
 # return channels given the name
 @app.get("/channels/{channel_name}")
-def get_channel(channel_name: str, token: Annotated[str, Depends(oauth2_scheme)], session: SessionDep):
+def get_channel(channel_name: str, session: SessionDep):
     """
     This endpoint returns all the channels with the given channel_name.
     params:
@@ -85,7 +74,7 @@ def get_channel(channel_name: str, token: Annotated[str, Depends(oauth2_scheme)]
 
 # create a channel
 @app.post("/channels", response_model=Channel)
-def create_channel_endpoint(channel_name: str, description: str, token: Annotated[str, Depends(oauth2_scheme)], session: SessionDep):
+def create_channel_endpoint(channel_name: str, description: str, session: SessionDep):
     """
     This endpoint creates a channel with the given channel_name and description.
     params:
@@ -100,21 +89,12 @@ def create_channel_endpoint(channel_name: str, description: str, token: Annotate
 
 # join a channel
 @app.post("/channels/join")
-def join_channel_endpoint(channel_id: int, token: Annotated[str, Depends(oauth2_scheme)], session: SessionDep):
+def join_channel_endpoint(channel_id: int, username: str, session: SessionDep):
     """
     This endpoint joins a channel with the given channel_id and the username.
     params:
         channel_id int: the channel_id given by the user
     """
-    try:
-        username = get_username_from_token(token)
-    except Exception as error:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-
     try:
         user_channel = join_channel(channel_id, username, session)
         return {"status": "success", "channel_id": user_channel}
